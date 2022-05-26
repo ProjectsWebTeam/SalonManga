9b388c9af70c9991129b6fc0d1371a73e8284881
Edusk <eduardo.campos.am@gmail.com>
2022-05-26 15:31:13 -0600

    commit

--- index.php
+++ index.php
@@ -3,8 +3,4 @@
     echo "Hola desde aqui";
     echo "Bienvienidos a SalonManga";
     echo "Casualidades";
-<<<<<<< HEAD
-=======
-    echo "Causalidades";
->>>>>>> 28444edde938a150ae630863857ad87148020e07
 ?>
