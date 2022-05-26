28444edde938a150ae630863857ad87148020e07
Edusk <eduardo.campos.am@gmail.com>
2022-05-26 15:26:11 -0600

    Mensajes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     echo "Hola mundo";
     echo "Hola desde aqui";
-    echo "Bienvienidos a SalonManga"
-    echo "Casualidades"
+    echo "Bienvienidos a SalonManga";
+    echo "Casualidades";
+    echo "Causalidades";
 ?>
