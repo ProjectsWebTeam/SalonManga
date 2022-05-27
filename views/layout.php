a3fb84db148ca5c8220e3f2de76a2121daa9b4fa
rodrigotobar11 <39451589+rodrigotobar11@users.noreply.github.com>
2022-05-26 21:21:43 -0600

    commit

--- views/layout.php
+++ views/layout.php
@@ -25,28 +25,28 @@
     <?php echo $contenido; ?>
     <div class="grid-container">
     <div class="card">
-      <img src="img_avatar.png" alt="Avatar" style="width:100%">
+      <img src="../src/img/1.jpg" alt="Avatar" style="width:18rem">
         <div class="container">
            <h4><b>Nombre</b></h4> 
            <p>Descripción</p> 
         </div>
     </div>
     <div class="card">
-      <img src="img_avatar.png" alt="Avatar" style="width:100%">
+      <img src="../src/img/2.jpg" alt="Avatar" style="width:20rem">
         <div class="container">
             <h4><b>Nombre</b></h4> 
            <p>Descripción</p> 
         </div>
     </div>
     <div class="card">
-      <img src="img_avatar.png" alt="Avatar" style="width:100%">
+      <img src="../src/img/3.jpg" alt="Avatar" style="width:18rem">
         <div class="container">
            <h4><b>Nombre</b></h4> 
            <p>Descripción</p> 
         </div>
     </div>
     <div class="card">
-      <img src="img_avatar.png" alt="Avatar" style="width:100%">
+      <img src="../src/img/4.jpg" alt="Avatar" style="width:18rem">
         <div class="container">
            <h4><b>Nombre</b></h4> 
            <p>Descripción</p> 
