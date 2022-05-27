0fc53a2edf71b6db8ce0325a320e354749c7e25c
Edusk <eduardo.campos.am@gmail.com>
2022-05-26 22:43:40 -0600

    commit

--- views/paginas/ultimos.php
+++ views/paginas/ultimos.php
@@ -2,7 +2,9 @@
     <div class="margin">
         <div class="galeriaMangas">
             <div class="cardUl">
-                <div class="imgUl"></div>
+                <div class="imgUl">
+                    <img src="src/img/1.jpg" alt="">
+                </div>
                 <div class="cardInfo">
                     <p class="tituloCard">DanDaDan</p>
                     <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
@@ -12,7 +14,21 @@
             </div><!-- Fin del Card -->
 
             <div class="cardUl">
-                <div class="imgUl"></div>
+                <div class="imgUl">
+                    <img src="src/img/1.jpg" alt="">
+                </div>
+                <div class="cardInfo">
+                    <p class="tituloCard">DanDaDan</p>
+                    <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
+
+                    <a href="#" class="btnCard">Leer Capitulos</a>
+                </div>
+            </div><!-- Fin del Card -->
+            
+            <div class="cardUl">
+                <div class="imgUl">
+                    <img src="src/img/1.jpg" alt="">
+                </div>
                 <div class="cardInfo">
                     <p class="tituloCard">DanDaDan</p>
                     <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
@@ -22,7 +38,9 @@
             </div><!-- Fin del Card -->
 
             <div class="cardUl">
-                <div class="imgUl"></div>
+                <div class="imgUl">
+                    <img src="src/img/1.jpg" alt="">
+                </div>
                 <div class="cardInfo">
                     <p class="tituloCard">DanDaDan</p>
                     <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
@@ -32,7 +50,9 @@
             </div><!-- Fin del Card -->
 
             <div class="cardUl">
-                <div class="imgUl"></div>
+                <div class="imgUl">
+                    <img src="src/img/1.jpg" alt="">
+                </div>
                 <div class="cardInfo">
                     <p class="tituloCard">DanDaDan</p>
                     <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
@@ -40,16 +60,7 @@
                     <a href="#" class="btnCard">Leer Capitulos</a>
                 </div>
             </div><!-- Fin del Card -->
-
-            <div class="cardUl">
-                <div class="imgUl"></div>
-                <div class="cardInfo">
-                    <p class="tituloCard">DanDaDan</p>
-                    <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
-
-                    <a href="#" class="btnCard">Leer Capitulos</a>
-                </div>
-            </div><!-- Fin del Card -->
+            
         </div>
 
     </div>
