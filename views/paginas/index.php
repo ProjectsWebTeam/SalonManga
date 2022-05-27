7f703545bea85f94ac1d41628c130b9b5e9f7a0a
Edusk <eduardo.campos.am@gmail.com>
2022-05-26 21:11:56 -0600

    commit

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -1,12 +1,3 @@
 <main>
-    <div class="cardManga">
-        <div class="imagenManga"></div>
-
-        <div class="infoManga">
-            <p class="tituloM">Nombre</p>
-            <p class="descM">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, magnam numquam optio suscipit cumque minima sit, nemo explicabo praesentium corrupti voluptate sapiente provident magni harum eius voluptatem enim, animi laborum.</p>
-
-            <a href="#" class="btnManga">Leer capitulos</a>
-        </div>
-    </div>
+    
 </main>
