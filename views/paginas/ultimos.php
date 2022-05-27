344348d8ff4ec74ddef6502e46d846946211e737
Edusk <eduardo.campos.am@gmail.com>
2022-05-26 21:47:27 -0600

    commit

---
+++ views/paginas/ultimos.php
@@ -0,0 +1,9 @@
+<main>
+    <div class="cardUl">
+        <div class="imgUl"></div>
+        <div class="cardInfo">
+            <p class="tituloCard">Nombre</p>
+            <p class="decripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde, similique quod expedita dolorem officiis laborum, doloremque voluptas asperiores aperiam tempore tempora.</p>
+        </div>
+    </div>
+</main>
