5a9a63fc86552ac208c554648d38ec302104db83
rodrigotobar11 <39451589+rodrigotobar11@users.noreply.github.com>
2022-05-26 21:29:47 -0600

    commit

--- views/layout.php
+++ views/layout.php
@@ -23,36 +23,7 @@
     </header>
     
     <?php echo $contenido; ?>
-    <div class="grid-container">
-    <div class="card">
-      <img src="../src/img/1.jpg" alt="Avatar" style="width:18rem">
-        <div class="container">
-           <h4><b>Nombre</b></h4> 
-           <p>Descripción</p> 
-        </div>
-    </div>
-    <div class="card">
-      <img src="../src/img/2.jpg" alt="Avatar" style="width:20rem">
-        <div class="container">
-            <h4><b>Nombre</b></h4> 
-           <p>Descripción</p> 
-        </div>
-    </div>
-    <div class="card">
-      <img src="../src/img/3.jpg" alt="Avatar" style="width:18rem">
-        <div class="container">
-           <h4><b>Nombre</b></h4> 
-           <p>Descripción</p> 
-        </div>
-    </div>
-    <div class="card">
-      <img src="../src/img/4.jpg" alt="Avatar" style="width:18rem">
-        <div class="container">
-           <h4><b>Nombre</b></h4> 
-           <p>Descripción</p> 
-        </div>
-    </div>
-    </div>
+    
     <footer class="footer">
         <div>
             <p>&copy;2022. Derechos Reservados</p>
--- views/paginas/index.php
+++ views/paginas/index.php
@@ -1,3 +1,32 @@
 <main>
-    
+    <div class="grid-container">
+    <div class="card">
+      <img src="../src/img/1.jpg" alt="Avatar" style="width:18rem">
+        <div class="container">
+           <h4><b>Nombre</b></h4> 
+           <p>Descripción</p> 
+        </div>
+    </div>
+    <div class="card">
+      <img src="../src/img/2.jpg" alt="Avatar" style="width:20rem">
+        <div class="container">
+            <h4><b>Nombre</b></h4> 
+           <p>Descripción</p> 
+        </div>
+    </div>
+    <div class="card">
+      <img src="../src/img/3.jpg" alt="Avatar" style="width:18rem">
+        <div class="container">
+           <h4><b>Nombre</b></h4> 
+           <p>Descripción</p> 
+        </div>
+    </div>
+    <div class="card">
+      <img src="../src/img/4.jpg" alt="Avatar" style="width:18rem">
+        <div class="container">
+           <h4><b>Nombre</b></h4> 
+           <p>Descripción</p> 
+        </div>
+    </div>
+    </div>
 </main>
