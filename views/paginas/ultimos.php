1d4a0f2d3546fa2eabbf944160aefca657b17938
Edusk <eduardo.campos.am@gmail.com>
2022-05-26 22:28:43 -0600

    commit

--- views/paginas/ultimos.php
+++ views/paginas/ultimos.php
@@ -1,13 +1,56 @@
 <main class="contenedor">
     <div class="margin">
-        <div class="cardUl">
-            <div class="imgUl"></div>
-            <div class="cardInfo">
-                <p class="tituloCard">DanDaDan</p>
-                <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
+        <div class="galeriaMangas">
+            <div class="cardUl">
+                <div class="imgUl"></div>
+                <div class="cardInfo">
+                    <p class="tituloCard">DanDaDan</p>
+                    <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
 
-                <a href="#" class="btnCard">Leer Capitulos</a>
-            </div>
+                    <a href="#" class="btnCard">Leer Capitulos</a>
+                </div>
+            </div><!-- Fin del Card -->
+
+            <div class="cardUl">
+                <div class="imgUl"></div>
+                <div class="cardInfo">
+                    <p class="tituloCard">DanDaDan</p>
+                    <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
+
+                    <a href="#" class="btnCard">Leer Capitulos</a>
+                </div>
+            </div><!-- Fin del Card -->
+
+            <div class="cardUl">
+                <div class="imgUl"></div>
+                <div class="cardInfo">
+                    <p class="tituloCard">DanDaDan</p>
+                    <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
+
+                    <a href="#" class="btnCard">Leer Capitulos</a>
+                </div>
+            </div><!-- Fin del Card -->
+
+            <div class="cardUl">
+                <div class="imgUl"></div>
+                <div class="cardInfo">
+                    <p class="tituloCard">DanDaDan</p>
+                    <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
+
+                    <a href="#" class="btnCard">Leer Capitulos</a>
+                </div>
+            </div><!-- Fin del Card -->
+
+            <div class="cardUl">
+                <div class="imgUl"></div>
+                <div class="cardInfo">
+                    <p class="tituloCard">DanDaDan</p>
+                    <p class="descripCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nesciunt facilis excepturi adipisci voluptatum, corporis error nisi unde.</p>
+
+                    <a href="#" class="btnCard">Leer Capitulos</a>
+                </div>
+            </div><!-- Fin del Card -->
         </div>
+
     </div>
 </main>
