a44abfbdf4f408c66941b6092c751c3d1c8556f4
Edusk <eduardo.campos.am@gmail.com>
2022-05-28 23:35:31 -0600

    Agregada pagina de categorias

---
+++ views/paginas/categorias.php
@@ -0,0 +1,44 @@
+<main class="contenedorPrin">
+    <h1 class="text-center tituloPagina">Categorías</h1>
+    <div class="categoriasGrid">
+        <div class="cardCat">
+            <a href="#">
+                <img class="bannerManga" src="src/img/shonen.jpg" alt="shonen manga">
+
+                <div class="overlayCat">
+                    <p class="tituloCategoria">Shōnen</p>
+                </div>
+            </a>
+        </div><!-- Fin del card categoria -->
+
+        <div class="cardCat">
+            <a href="#">
+                <img class="bannerManga" src="src/img/shoujo.jpg" alt="shonen manga">
+
+                <div class="overlayCat">
+                    <p class="tituloCategoria">Shōjo</p>
+                </div>
+            </a>
+        </div><!-- Fin del card categoria -->
+
+        <div class="cardCat">
+            <a href="#">
+                <img class="bannerManga" src="src/img/horror.jpg" alt="shonen manga">
+
+                <div class="overlayCat">
+                    <p class="tituloCategoria">Horror</p>
+                </div>
+            </a>
+        </div><!-- Fin del card categoria -->
+
+        <div class="cardCat">
+            <a href="#">
+                <img class="bannerManga" src="src/img/seinen.jpg" alt="shonen manga">
+
+                <div class="overlayCat">
+                    <p class="tituloCategoria">Seinen</p>
+                </div>
+            </a>
+        </div><!-- Fin del card categoria -->
+    </div>
+</main>
