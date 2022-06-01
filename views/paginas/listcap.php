bd9db2d8357330ae59cdb07493f8f3f69437d234
rodrigotobar11 <39451589+rodrigotobar11@users.noreply.github.com>
2022-05-31 22:15:58 -0600

    commit

--- views/paginas/listcap.php
+++ views/paginas/listcap.php
@@ -1,10 +1,23 @@
 <main>
     <div class="contenedorImagen">
         <div class="image">
-            <img src="src/img/1.jpg" alt="">
+            <img class="portada" src="src/img/1.jpg" alt="">
         </div>
         <div class="text">
-            <h1>DanDaDan</h1>
+            <h2>Titulo: DanDaDan</h2>
+            <h3>Autor: Yukinobu Tatsu</h3>
+            <h3>Estado: Emisión</h3>
         </div>
+    </div>
+    <div class="container">
+        <ul>
+            <li><a class="btnCap" href="#">Dandadan - Capitulo 1</a></li>
+            <li><a class="btnCap" href="#">Dandadan - Capitulo 2</a></li>
+            <li><a class="btnCap" href="#">Dandadan - Capitulo 3</a></li>
+            <li><a class="btnCap" href="#">Dandadan - Capitulo 4</a></li>
+            <li><a class="btnCap" href="#">Dandadan - Capitulo 5</a></li>
+            <li><a class="btnCap" href="#">Dandadan - Capitulo 6</a></li>
+            <li><a class="btnCap" href="#">Dandadan - Capitulo 7</a></li>
+        </ul>
     </div>
 </main>
