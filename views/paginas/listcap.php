960162f1cdf14b26df9637cc705de0b7f6c73f67
rodrigotobar11 <39451589+rodrigotobar11@users.noreply.github.com>
2022-05-31 22:48:03 -0600

    commit

--- views/paginas/listcap.php
+++ views/paginas/listcap.php
@@ -1,6 +1,4 @@
 <main>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="contenedorImagen">
         <div class="image">
             <img class="portada" src="src/img/1.jpg" alt="">
